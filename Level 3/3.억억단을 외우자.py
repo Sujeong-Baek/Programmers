9f75f317bf061fac38e3b2eb17ff6696a9b4ee63
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-14 14:03:57 +0900

    update

--- Level 3/3.억억단을 외우자.py
+++ Level 3/3.억억단을 외우자.py
@@ -1,1 +1,22 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/138475
+def solution(e, starts):
+    answer = []
+    num2count={}
+    for num in range(1,e+1):
+        count = 0
+        for i in range(1, num + 1):
+            if num % i == 0:
+                count += 1
+        num2count[num]=count
+
+    for start in starts:
+        max_count=0
+        min_num=0
+
+        for num in range(e, start-1, -1):
+            if max_count<=num2count[num]:
+                max_count=num2count[num]
+                min_num=num
+        answer.append(min_num)
+            
+    return answer
