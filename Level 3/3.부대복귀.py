66b768e36a5b8c1c2d74c3aefde2672674c61a28
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-16 00:15:49 +0900

    update

--- Level 3/3.부대복귀.py
+++ Level 3/3.부대복귀.py
@@ -1,15 +1,31 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/132266
-from collections import deque
+from collections import deque, defaultdict
 def solution(n, roads, sources, destination):
     answer = []
-    graph = [[] for _ in range(n+1)]
+    graph = defaultdict(list)
     for n1, n2 in roads:
         graph[n1].append(n2)
         graph[n2].append(n1)    
-        
+
     for source in sources:
-        answer.append(bfs(graph, source, destination))
+        answer.append(bfs(graph, n, source, destination))
+        
     return answer
 
-def bfs(graph, source, destination):
-    pass
+def bfs(graph, n, source, destination):
+    invited = [-1]*(n+1)
+    invited[source] = 0
+    queue = deque([source])
+    
+    while queue:
+        node = queue.popleft()
+
+        if node == destination:
+            break
+        
+        for neighbor in graph[node]:
+            if invited[neighbor] == -1:
+                invited[neighbor] = invited[node] + 1
+                queue.append(neighbor)
+                
+    return invited[destination]
