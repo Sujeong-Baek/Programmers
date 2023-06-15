727a3715454d08d808e43fe0f5836550c8839245
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-15 23:25:20 +0900

    update

--- Level 3/3.부대복귀.py
+++ Level 3/3.부대복귀.py
@@ -1,1 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/132266
+from collections import deque
+def solution(n, roads, sources, destination):
+    answer = []
+    graph = [[] for _ in range(n+1)]
+    for n1, n2 in roads:
+        graph[n1].append(n2)
+        graph[n2].append(n1)    
+        
+    for source in sources:
+        answer.append(bfs(graph, source, destination))
+    return answer
+
+def bfs(graph, source, destination):
+    pass
