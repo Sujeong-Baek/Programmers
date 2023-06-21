004dd7827d5c5376b287cdc04fc972ae323b2442
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-21 17:44:22 +0900

    update

--- Level 3/3.최고의 집합.py
+++ Level 3/3.최고의 집합.py
@@ -1,1 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12938
+def solution(n, s):
+     quotient, remainder = divmod(s, n)
+     if quotient<1:
+          return [-1]
+     answer=[quotient]*n
+
+     for i in range(remainder):
+          answer[i]+=1
+     answer.sort()
+     return answer
