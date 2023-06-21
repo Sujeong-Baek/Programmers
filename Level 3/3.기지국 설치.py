e533df7be494087606593576bfd30ea2769b4b7e
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-21 19:14:22 +0900

    update

--- Level 3/3.기지국 설치.py
+++ Level 3/3.기지국 설치.py
@@ -1,1 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12979
+def solution(n, stations, w):
+    answer=0
+    apart=[0]*(n+1)
+
+    for station in stations:
+        start=max(station-w, 1)
+        end=min(station+w, n+1)
+        apart[start:end+1]=[1]*(end-start+1)
+
+    start=1
+    while start<=n:
+        if apart[start]==0:
+            end=min(start+2*w+1, n+1)
+            apart[start:end]=[1]*(end-start)
+            answer+=1
+
+    return answer
