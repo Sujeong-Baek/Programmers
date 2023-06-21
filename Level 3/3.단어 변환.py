c4e1ea2a84e038c8efc66073b9011263cc033f6d
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-21 18:20:16 +0900

    update

--- Level 3/3.단어 변환.py
+++ Level 3/3.단어 변환.py
@@ -1,1 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43163
+from collections import deque
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+
+    queue = deque([(begin, 0)]) 
+    while queue:
+        current_word, current_count= queue.popleft()
+        if current_word == target:
+            return current_count
+        
+        for word in words:
+            if len([c1 for c1,c2 in zip(current_word, word) if c1 != c2]) == 1:
+                words.remove(word)
+                queue.append((word, current_count+1))
+    return 0
