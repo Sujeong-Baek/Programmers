3f619353339724c5b6f97a7f9579c605b8f54f88
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-21 18:09:05 +0900

    update

--- Level 3/3.야근지수.py
+++ Level 3/3.야근지수.py
@@ -1,1 +1,16 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12927
+from heapq import heapify,heappop,heappush
+def solution(n, works):
+    if sum(works)<=n:
+        return 0
+    
+    works=[(-1)*work for work in works]
+    heapify(works)
+
+    while n>0:
+        max_work=heappop(works)
+        work = max_work+1
+        heappush(works, work)
+        n-=1
+        
+    return sum([work**2 for work in works])
