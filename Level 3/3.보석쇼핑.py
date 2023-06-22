e56fa06d9c0a24177ff490f7102de44df4c7b8de
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-22 15:37:11 +0900

    update

--- Level 3/3.보석쇼핑.py
+++ Level 3/3.보석쇼핑.py
@@ -1,1 +1,24 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/67258
+from collections import defaultdict
+def solution(gems):
+
+    gem_types=len(set(gems))
+    current_gems2count=defaultdict(int)
+    start=0
+    end=0
+    
+    while end<len(gems):
+        current_gems2count[gems[end]]+=1
+        end+=1 
+        
+        if len(current_gems2count)==gem_types:
+            while start<end:
+                if current_gems2count[gems[start]]>1:
+                    current_gems2count[gems[start]]-=1
+                    start+=1
+                else:
+                    break
+                    
+            break
+    
+    return [start+1, end]
