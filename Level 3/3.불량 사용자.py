3bf93bf7d546a0e4eefa7701645a581eacee63b4
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-22 14:00:22 +0900

    update

--- Level 3/3.불량 사용자.py
+++ Level 3/3.불량 사용자.py
@@ -1,1 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/64064
+def solution(user_id, banned_id):
+    answer = 0
+    banned_ids=[[] for _ in range(len(banned_id))]
+
+    for idx, ban in enumerate(banned_id):        
+        for num, user in enumerate(user_id):
+            if len(ban)==len(user) and all(b=='*' or b==u for b, u in zip(ban, user)):
+                banned_ids[idx].append(num)
+
+    return answer
