df4f25f120b939f21a2479f36a1438f9643aa80f
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-27 23:52:58 +0900

    update

--- Level 3/3.스티커 모으기(2).py
+++ Level 3/3.스티커 모으기(2).py
@@ -1,1 +1,24 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12971
+def solution(sticker):
+    answer1 = 0
+    answer2 = 0
+    
+    if len(sticker)%2 !=0:
+        for idx, s in enumerate(sticker[1:],1):
+            if idx%2 == 0 and idx != len(sticker)-1:
+                answer1+=s
+
+            elif idx%2 != 0 and idx != len(sticker)-1:
+                answer2+=s
+                
+        answer1+=max(sticker[0],sticker[-1])
+        
+    else:
+        for idx, s in enumerate(sticker):
+            if idx%2 == 0:
+                answer1+=s
+
+            elif idx%2 != 0:
+                answer2+=s
+    return max(answer1, answer2)
+
