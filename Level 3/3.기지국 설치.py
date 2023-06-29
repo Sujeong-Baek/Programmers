dcb2409de0f3640568b64b6ee4913e6b8ad73dc4
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-29 17:21:30 +0900

    udpate

--- Level 3/3.기지국 설치.py
+++ Level 3/3.기지국 설치.py
@@ -1,18 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12979
 def solution(n, stations, w):
     answer = 0
-    current = 1 # current번을 이제 확인해야함
-    idx = 0 # stations[idx] 가 다음 기지국
-    while current <= n: 
-        if current < stations[idx] - w:
-            answer += 1 # 1
-            current += 1+ 2*w # 4
-        elif idx < len(stations)-1:
-            current=stations[idx]+w+1 #8
-            idx+=1 #2
-        else:
-            answer +=1
-            current += 1+ 2*w
+    current = 0 # current번을 이제 확인해야함
+      
+    for station in stations:
+        while current < station-w-1:
+            answer += 1
+            current += 1 + 2*w
+        current = station+w
+        
+    while current < n:
+            answer += 1
+            current += 1 + 2*w      
+            
     return answer
 
 # 기지국과 연결되는 가장 끝부분을 저장해두고 갱신
