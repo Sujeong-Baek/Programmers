5aea204f98611e996a387d7b03d536f448f08235
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-30 23:28:04 +0900

    update

--- Level 3/3.징검다리 건너기.py
+++ Level 3/3.징검다리 건너기.py
@@ -1,1 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/64062#
+def solution(stones, k):
+    answer = 0
+    go=True
+    while go:
+        for idx, stone in enumerate(stones):
+            if stone > 0 :
+                s = 0
+                stones[idx]-=1
+            else:
+                s += 1
+                if s == k:
+                    go=False
+                    break
+        if not go:
+            break
+        answer+=1
+    return answer
