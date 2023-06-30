313edb57fa736014c6a22f000c0bf5af832acfdf
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-06-30 23:39:20 +0900

    시간초과

--- Level 3/3.징검다리 건너기.py
+++ Level 3/3.징검다리 건너기.py
@@ -1,18 +1,21 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/64062#
 def solution(stones, k):
-    answer = 0
-    go=True
-    while go:
-        for idx, stone in enumerate(stones):
-            if stone > 0 :
-                s = 0
-                stones[idx]-=1
-            else:
-                s += 1
-                if s == k:
-                    go=False
+    left = 0
+    right = max(stones)
+
+    while left < right:
+        mid = (left+right) // 2
+        zeros = [0 if stone <= mid else 1 for stone in stones]
+
+        for idx, zero in enumerate(zeros):
+            if zero == 0:
+                i = idx
+                while i < len(stones) and zeros[i] == 0:
+                    i += 1
+                if i - idx >= k:
+                    right = mid
                     break
-        if not go:
-            break
-        answer+=1
-    return answer
+        else:
+            left = mid + 1
+
+    return right
