92dc19ee852d068b1ae7fe0bacd1e99e8e92d07f
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-07-02 21:35:33 +0900

    update

--- Level 3/3.억억단을 외우자.py
+++ Level 3/3.억억단을 외우자.py
@@ -1,22 +1,32 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/138475
+from collections import defaultdict
 def solution(e, starts):
-    answer = []
-    num2count={}
+    num2count=defaultdict(int)
     for num in range(1,e+1):
-        count = 0
-        for i in range(1, num + 1):
-            if num % i == 0:
-                count += 1
-        num2count[num]=count
+        for i in range(num, e+1, num):            
+            num2count[i] += 1
 
-    for start in starts:
-        max_count=0
-        min_num=0
+    num2min_num=defaultdict(int)
+    for num in range(e, min(starts)-1, -1):        
+        if max_count<=num2count[num]:
+            max_count=num2count[num]
+            min_num=num
+        num2min_num[num] = min_num
+                        
+    return [num2min_num[start] for start in starts]
 
-        for num in range(e, start-1, -1):
-            if max_count<=num2count[num]:
-                max_count=num2count[num]
-                min_num=num
-        answer.append(min_num)
-            
-    return answer
+# 6: 4 - [1,2,3,6]
+# 6 % 6 == 0
+# 6 % 5 != 0
+# ...
+# n번
+
+# 1 * 6 == 6, + 1
+
+# 2 * 1 == 2, num2count[2] += 1
+# 2 * 2 == 4, num2count[4] += 1
+# 2 * 3 == 6, num2count[6] += 1
+# 2 * 4 == 8, num2count[8] += 1
+
+# 3 * 2 == 6, + 1
+# 6 * 1 == 6, + 1
