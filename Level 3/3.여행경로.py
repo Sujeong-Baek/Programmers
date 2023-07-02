793d4342399b93a8804acd3da3ebf9b7bdb2b6c2
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-07-02 20:06:39 +0900

    update

--- Level 3/3.여행경로.py
+++ Level 3/3.여행경로.py
@@ -1,6 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43164
 from collections import defaultdict
-def soluton(tickets):
+def solution(tickets):
     answer = []
     depart2arrive = defaultdict(list)
     for depart, arrive in tickets:
@@ -12,12 +12,9 @@ def soluton(tickets):
     stack = ['ICN']
     while stack:
         top = stack[-1]
-        
         if top not in depart2arrive or len(depart2arrive[top]) == 0:
             answer.append(stack.pop())
-        
         else:
             stack.append(depart2arrive[top][-1])
             depart2arrive[top] = depart2arrive[top][:-1]
-
-    return answer
+    return answer[::-1]
