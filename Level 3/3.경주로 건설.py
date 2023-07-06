4f73fd21a77b8c86dfebf82b4ce85e1fb9bfa21e
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-07-06 12:26:15 +0900

    update

--- Level 3/3.경주로 건설.py
+++ Level 3/3.경주로 건설.py
@@ -3,28 +3,24 @@ from collections import deque
 def solution(board):
 
     N = len(board)
-    directions = {'R':(0, 1), 'L':(0, -1), 'U':(-1, 0), 'D':(1, 0)}
+    directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
     
     que = deque([(0,0,'d',0)])
-    min_cost = [[float('inf')] * N for _ in range(N)]
-    min_cost[0][0] = 0
+    costs = [[[float('inf')]*4 for _ in range(N)] for _ in range(N)]
+    costs[0][0] = [0,0,0,0]
        
     while que:
         r, c, d, cost = que.popleft()
         
-        for direction, (dr, dc) in directions.items():            
+        for direction, (dr, dc) in enumerate(directions):            
             nr = r + dr
             nc = c + dc
             
-            if 0<= nr < N and 0<= nc < N and board[nr][nc] == 0:     
-        
-                if d != direction:
-                    current_cost = cost + 600
-                elif d == direction:
-                    current_cost = cost + 100  
-                
-                if min_cost[nr][nc] >= current_cost:                     
+            if 0<= nr < N and 0<= nc < N and board[nr][nc] == 0:          
+                current_cost = cost + 100 if d == direction else cost + 600
+            
+                if costs[nr][nc][direction]>current_cost:
+                    costs[nr][nc][direction] = current_cost
                     que.append((nr, nc, direction, current_cost))
-                    min_cost[nr][nc] = current_cost
-                   
-    return min_cost[-1][-1] - 500
+
+    return min(costs[-1][-1]) - 500
