c92fc3986120b1fa00000bb961e5ee718825cd41
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-07-07 14:22:37 +0900

    update

--- Level 3/3.입국심사.py
+++ Level 3/3.입국심사.py
@@ -1,1 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43238
+def solution(n, times):
+    mini = 0
+    maxi = min(times) * n
+    
+    while mini < maxi:
+        mid = (mini+maxi)//2
+        total = sum([mid//time for time in times]) 
+        
+        if total < n:
+            mini = mid+1
+        else:
+            maxi = mid  
+            
+    return maxi
